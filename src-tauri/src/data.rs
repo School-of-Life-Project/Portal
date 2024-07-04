f5a39640245f42d59a6673512b455e8fc9eb6936
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-04 08:36:31 -0700

    Added basic file management

--- src-tauri/src/data.rs
+++ src-tauri/src/data.rs
@@ -1,19 +1,21 @@
-use std::path::Path;
+use std::{
+    io::ErrorKind,
+    path::{Path, PathBuf},
+};
 
 use derive_more::{Display, Error, From};
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use tokio::{
-    fs::{self, File, ReadDir},
+    fs::{self, DirEntry, File, ReadDir},
     io::{self, AsyncReadExt},
     task,
 };
+use uuid::Uuid;
 
-/*#[derive(From, Debug, Display, Error)]
-pub enum DataError {
-    ioError(io::Error),
-    tomlError(toml::de::Error),
-}*/
+pub fn get_data_dir(app_handle: tauri::AppHandle) -> Option<PathBuf> {
+    app_handle.path_resolver().app_data_dir()
+}
 
 async fn ensure_folder_exists(path: &Path) -> Result<(), io::Error> {
     let metadata = fs::metadata(path).await;
@@ -33,9 +35,16 @@ async fn ensure_folder_exists(path: &Path) -> Result<(), io::Error> {
     Ok(())
 }
 
+pub async fn deserialize_file<T>(path: &Path) -> Result<T, DataError>
+where
+    T: DeserializeOwned,
+{
+    let data = fs::read_to_string(path).await?;
+    Ok(toml::from_str(&data)?)
+}
+
 pub async fn deserialize_config_folder<F, T>(
     path: &Path,
-    callback: F,
 ) -> Result<Vec<Result<T, toml::de::Error>>, io::Error>
 where
     F: Fn(&Path) -> bool,
@@ -49,7 +58,7 @@ where
     while let Some(entry) = entries.next_entry().await? {
         let path = entry.path();
 
-        if callback(&path) {
+        if path.ends_with(".toml") {
             let data = fs::read_to_string(&path).await?;
             items.push(toml::from_str(&data));
         }
@@ -58,12 +67,77 @@ where
     Ok(items)
 }
 
+/*
+Plans:
+- Course maps: No sorting required
+- Courses: Organized by UUID
+
+*/
+
+#[derive(From, Debug, Display, Error)]
+pub enum DataError {
+    ioError(io::Error),
+    tomlError(toml::de::Error),
+}
+
+pub struct ResourceManager {
+    pub root: PathBuf,
+}
+
+impl ResourceManager {
+    pub async fn new(root: PathBuf) -> Result<Self, DataError> {
+        ensure_folder_exists(&root).await?;
+
+        Ok(Self { root })
+    }
+
+    pub async fn get(&self, id: Uuid) -> Option<PathBuf> {
+        let path = self.root.join(format!("{}", id));
+
+        match fs::metadata(&path).await {
+            Ok(metadata) => match metadata.is_dir() {
+                true => Some(path),
+                false => None,
+            },
+            Err(_) => None,
+        }
+    }
+    pub async fn scan(&self) -> Result<Vec<Uuid>, DataError> {
+        let mut items = Vec::new();
+
+        let mut entries = fs::read_dir(&self.root).await?;
+        while let Some(entry) = entries.next_entry().await? {
+            let path = entry.path();
+
+            let is_dir = match fs::metadata(&path).await {
+                Ok(metadata) => metadata.is_dir(),
+                Err(_) => false,
+            };
+
+            if is_dir {
+                let uuid = Uuid::try_parse(&path.file_name().unwrap_or_default().to_string_lossy());
+
+                match uuid {
+                    Ok(uuid) => items.push(uuid),
+                    Err(_) => {
+                        let uuid = Uuid::new_v4();
+                        let dest = path.with_file_name(format!("{}", uuid));
+
+                        fs::rename(path, dest).await?;
+
+                        items.push(uuid)
+                    }
+                }
+            }
+        }
+
+        Ok(items)
+    }
+}
+
 #[tauri::command]
 pub async fn open_data_dir(app_handle: tauri::AppHandle) -> Result<(), String> {
-    let path = app_handle
-        .path_resolver()
-        .app_data_dir()
-        .ok_or("Unable to find application data directory")?;
+    let path = get_data_dir(app_handle).ok_or("Unable to find application data directory")?;
 
     ensure_folder_exists(&path)
         .await
