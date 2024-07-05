c2ed2dd844d56153897ac21430a046ee49f4f392
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-05 03:23:36 -0700

    Changed file locking mechanism

--- src-tauri/src/data.rs
+++ src-tauri/src/data.rs
@@ -1,18 +1,16 @@
 use std::{
-    collections::HashMap,
     env,
     ffi::OsString,
     path::{Path, PathBuf},
-    sync::{Arc, Weak},
 };
 
+use advisory_lock::{AdvisoryFileLock, FileLockError, FileLockMode};
 use derive_more::{Display, Error, From};
-
 use serde::{de::DeserializeOwned, Serialize};
 use tokio::{
     fs::{self, File, OpenOptions},
     io::{self, AsyncReadExt, AsyncWriteExt},
-    sync::{Mutex, OwnedMutexGuard},
+    task::{self, JoinError},
 };
 use toml::{Deserializer, Serializer};
 use uuid::{fmt::Simple, Uuid};
@@ -48,21 +46,71 @@ pub async fn ensure_folder_exists(path: &Path) -> Result<(), io::Error> {
     Ok(())
 }
 
-/// A custom Error type for errors returned by this module.
+/// An error encountered when locking a ConfigFile
+#[derive(From, Debug, Display, Error)]
+pub enum LockError {
+    Tokio(JoinError),
+    Sys(FileLockError),
+}
+
+async fn lock_file(file: File, mode: FileLockMode) -> Result<File, LockError> {
+    let std_file = file.into_std().await;
+
+    let file = task::spawn_blocking(move || -> Result<File, FileLockError> {
+        std_file.lock(mode)?;
+
+        Ok(File::from_std(std_file))
+    })
+    .await??;
+
+    Ok(file)
+}
+
+/// A custom Error type for ConfigFile.
 #[derive(From, Debug, Display, Error)]
 pub enum DataError {
     Io(io::Error),
     TomlDe(toml::de::Error),
     TomlSe(toml::ser::Error),
+    Lock(LockError),
 }
 
-/// A read-write manager of TOML configuration files.
+/// A read-only handle to a TOML configuration file.
 pub struct ConfigFile {
     pub file: File,
     buffer: String,
 }
 
 impl ConfigFile {
+    pub async fn new(path: &Path) -> Result<Self, DataError> {
+        let file = File::open(path).await?;
+        let file = lock_file(file, FileLockMode::Shared).await?;
+        let metadata = file.metadata().await?;
+
+        Ok(Self {
+            file,
+            buffer: String::with_capacity(metadata.len().try_into().unwrap_or_default()),
+        })
+    }
+    pub async fn read<T>(&mut self) -> Result<T, DataError>
+    where
+        T: DeserializeOwned,
+    {
+        self.buffer.clear();
+        self.file.read_to_string(&mut self.buffer).await?;
+
+        let deserializer = Deserializer::new(&self.buffer);
+        Ok(T::deserialize(deserializer)?)
+    }
+}
+
+/// A read-write handle to a TOML configuration file.
+pub struct WritableConfigFile {
+    pub file: File,
+    buffer: String,
+}
+
+impl WritableConfigFile {
     pub async fn new(path: &Path) -> Result<Self, DataError> {
         let file = OpenOptions::new()
             .create(true)
@@ -71,6 +119,7 @@ impl ConfigFile {
             .write(true)
             .open(path)
             .await?;
+        let file = lock_file(file, FileLockMode::Exclusive).await?;
         let metadata = file.metadata().await?;
 
         Ok(Self {
@@ -104,117 +153,37 @@ impl ConfigFile {
     }
 }
 
-/// DataManager is a read-write manager of UUID-indexed ConfigFile objects.
-///
-/// Each ConfigFile maps to a TOML file within the DataManager's root folder.
+/// DataManager is a UUID-indexed file manager.
 pub struct DataManager {
     pub root: PathBuf,
-    files: Arc<Mutex<HashMap<Uuid, Weak<Mutex<ConfigFile>>>>>,
-    extension: OsString,
+    pub extension: Option<OsString>,
 }
 
 impl DataManager {
-    pub async fn new(root: PathBuf) -> Result<Self, DataError> {
+    pub async fn new(root: PathBuf, mut extension: Option<OsString>) -> Result<Self, io::Error> {
         ensure_folder_exists(&root).await?;
 
-        Ok(Self {
-            root,
-            files: Arc::new(Mutex::new(HashMap::new())),
-            extension: OsString::from("toml"),
-        })
-    }
-    async fn new_file(&self, id: Uuid) -> Result<ConfigFile, DataError> {
-        let mut path = self
-            .root
-            .join(Simple::from_uuid(id).encode_lower(&mut Uuid::encode_buffer()));
-        path.set_extension(OsString::from(&self.extension));
-
-        ConfigFile::new(&path).await
-    }
-    pub async fn get(&self, id: Uuid) -> Result<OwnedMutexGuard<ConfigFile>, DataError> {
-        let mut files = self.files.lock().await;
-
-        let mutex = match files.get(&id).and_then(|file| file.upgrade()) {
-            Some(file) => file.clone(),
-            None => {
-                let new = Arc::new(Mutex::new(self.new_file(id).await?));
-
-                files.insert(id, Arc::downgrade(&new));
-
-                new
-            }
-        };
-
-        Ok(mutex.lock_owned().await)
-    }
-    pub async fn scan(&self) -> Result<Vec<Uuid>, DataError> {
-        let mut items = Vec::new();
-        let mut buffer = Uuid::encode_buffer();
-
-        let mut entries = fs::read_dir(&self.root).await?;
-        while let Some(entry) = entries.next_entry().await? {
-            let path = entry.path();
-
-            let is_file = match fs::metadata(&path).await {
-                Ok(metadata) => metadata.is_file(),
-                Err(_) => false,
-            };
-
-            if !is_file || path.extension() != Some(&self.extension) {
-                continue;
-            }
-
-            let filestem = path.file_stem().unwrap_or_default().to_string_lossy();
-
-            let uuid = Uuid::try_parse(&filestem).unwrap_or_else(|_| Uuid::new_v4());
-            let formatted = Simple::from_uuid(uuid).encode_lower(&mut buffer);
-
-            if *formatted != *filestem {
-                let mut new_path = path.with_file_name(formatted);
-                new_path.set_extension(&self.extension);
-                fs::rename(&path, new_path).await?;
-            }
-
-            items.push(uuid);
+        if let Some(extension) = &mut extension {
+            extension.make_ascii_lowercase();
         }
 
-        Ok(items)
+        Ok(Self { root, extension })
     }
-}
-
-/// A read-only version of DataManager, optimized for reading large numbers of files.
-pub struct BasicDataManager {
-    pub root: PathBuf,
-    extension: OsString,
-}
-
-impl BasicDataManager {
-    pub async fn new(root: PathBuf) -> Result<Self, DataError> {
-        ensure_folder_exists(&root).await?;
-
-        Ok(Self {
-            root,
-            extension: OsString::from("toml"),
-        })
-    }
-    pub async fn get<T>(&self, id: Uuid) -> Result<T, DataError>
-    where
-        T: DeserializeOwned,
-    {
+    pub async fn get(&self, id: Uuid) -> Option<PathBuf> {
         let mut path = self
             .root
             .join(Simple::from_uuid(id).encode_lower(&mut Uuid::encode_buffer()));
-        path.set_extension(OsString::from(&self.extension));
+        if let Some(extension) = &self.extension {
+            path.set_extension(extension);
+        }
 
-        let mut file = File::open(path).await?;
-        let metadata = file.metadata().await?;
-
-        let mut buffer = String::with_capacity(metadata.len().try_into().unwrap_or_default());
-
-        file.read_to_string(&mut buffer).await?;
-
-        let deserializer = Deserializer::new(&buffer);
-        Ok(T::deserialize(deserializer)?)
+        match fs::metadata(&path).await {
+            Ok(metadata) => match metadata.is_dir() {
+                true => Some(path),
+                false => None,
+            },
+            Err(_) => None,
+        }
     }
     pub async fn scan(&self) -> Result<Vec<Uuid>, io::Error> {
         let mut items = Vec::new();
@@ -229,7 +198,9 @@ impl BasicDataManager {
                 Err(_) => false,
             };
 
-            if !is_file || path.extension() != Some(&self.extension) {
+            let extension = path.extension();
+
+            if !is_file || extension.map(|e| e.to_ascii_lowercase()) != self.extension {
                 continue;
             }
 
@@ -238,9 +209,11 @@ impl BasicDataManager {
             let uuid = Uuid::try_parse(&filestem).unwrap_or_else(|_| Uuid::new_v4());
             let formatted = Simple::from_uuid(uuid).encode_lower(&mut buffer);
 
-            if *formatted != *filestem {
+            if *formatted != *filestem || extension != self.extension.as_deref() {
                 let mut new_path = path.with_file_name(formatted);
-                new_path.set_extension(&self.extension);
+                if let Some(extension) = &self.extension {
+                    new_path.set_extension(extension);
+                }
                 fs::rename(&path, new_path).await?;
             }
 
@@ -251,9 +224,7 @@ impl BasicDataManager {
     }
 }
 
-/// ResourceManager is a read-only resource bundle manager.
-///
-/// Resource bundles are folders with a valid Uuid as their name.
+/// ResourceManager is a UUID-indexed folder manager.
 pub struct ResourceManager {
     pub root: PathBuf,
 }
