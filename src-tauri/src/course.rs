d4d316f793e97f9da1170aa2c6a34b062b6c3255
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-05 04:05:35 -0700

    Started work on implementing State

--- src-tauri/src/course.rs
+++ src-tauri/src/course.rs
@@ -1,12 +1,48 @@
-use std::path::{Path, PathBuf};
+use std::{
+    ffi::OsString,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
 use serde::{Deserialize, Serialize};
 use tokio::{
     fs::{self, ReadDir},
     io,
+    sync::{Mutex, OnceCell},
 };
 
-use crate::data;
+use crate::data::{self, ConfigFile, DataError, DataManager, ResourceManager, WritableConfigFile};
+
+pub struct State {
+    data_dir: PathBuf,
+    course_maps: DataManager,
+    courses: ResourceManager,
+    progress: DataManager,
+    active_courses: Mutex<WritableConfigFile>,
+    settings: Mutex<WritableConfigFile>,
+}
+
+impl State {
+    pub async fn new() -> Result<Self, DataError> {
+        let data_dir = data::get_data_dir(crate::IDENTIFIER).await?;
+
+        let extension = Some(OsString::from("toml"));
+        let course_map_path = data_dir.join("Course Maps");
+        let course_path = data_dir.join("Courses");
+        let progress_path = data_dir.join("Progress");
+        let active_courses_path = data_dir.join("Active Courses.toml");
+        let settings_path = data_dir.join("Settings.toml");
+
+        Ok(Self {
+            data_dir,
+            course_maps: DataManager::new(course_map_path, extension.clone()).await?,
+            courses: ResourceManager::new(course_path).await?,
+            progress: DataManager::new(progress_path, extension).await?,
+            active_courses: Mutex::new(WritableConfigFile::new(&active_courses_path).await?),
+            settings: Mutex::new(WritableConfigFile::new(&settings_path).await?),
+        })
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Course {
@@ -31,10 +67,10 @@ struct Chapter {
 pub async fn get_courses(
     window: tauri::Window,
     app_handle: tauri::AppHandle,
+    state: tauri::State<'_, State>,
 ) -> Result<Vec<Course>, String> {
-    let mut path =
-        data::get_data_dir(app_handle).ok_or("Unable to find application data directory")?;
-    path.push("courses");
+    //let mut path = data::get_data_dir(app_handle).ok_or("Unable to find application data directory")?;
+    //path.push("courses");
 
     //for entry in fs::read_dir(data_dir).await {}
 
@@ -46,3 +82,15 @@ pub async fn get_courses(
 
     todo!();
 }
+
+/*#[tauri::command]
+pub async fn open_data_dir(app_handle: tauri::AppHandle) -> Result<(), String> {
+    let path = get_data_dir(app_handle).ok_or("Unable to find application data directory")?;
+
+    ensure_folder_exists(&path)
+        .await
+        .map_err(|err| format!("Unable to open application data directory: {}", err))?;
+
+    open::that_detached(&path)
+        .map_err(|err| format!("Unable to launch system file opener: {}", err))
+}*/
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,28 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tokio::task;
+
 mod course;
 mod data;
 
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            //data::open_data_dir,
-            course::get_courses
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+const IDENTIFIER: &str = "com.schoolOfLifeProject.Portal";
+
+#[tokio::main]
+async fn main() {
+    let state = course::State::new()
+        .await
+        .expect("error when initalizing application directories");
+
+    task::spawn_blocking(move || {
+        tauri::Builder::default()
+            .manage(state)
+            .invoke_handler(tauri::generate_handler![
+                //data::open_data_dir,
+                course::get_courses
+            ])
+            .run(tauri::generate_context!())
+    })
+    .await
+    .expect("error when starting tauri task")
+    .expect("error when starting tauri");
 }
