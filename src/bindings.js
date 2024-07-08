c6dc934500910a0340b9cf2bd212bbb1cf374918
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-07 23:53:15 -0700

    Cleaned up bindings, bug fixes

--- src/bindings.js
+++ src/bindings.js
@@ -1,13 +1,15 @@
+import { invoke, convertFileSrc } from '@tauri-apps/api/tauri';
+
 export function openDataDir() {
-	return window.__TAURI_INVOKE__("open_data_dir");
+	return invoke("open_data_dir");
 }
 
 export function getCourseMaps() {
-	return window.__TAURI_INVOKE__("get_course_maps");
+	return invoke("get_course_maps");
 }
 
 export function getCourses() {
-	return window.__TAURI_INVOKE__("get_courses").then((courses) => {
+	return invoke("get_courses").then((courses) => {
 		if (Array.isArray(courses)) {
 			for (const course of courses) {
 				if (course.Ok) {
@@ -20,7 +22,7 @@ export function getCourses() {
 }
 
 export function getCoursesActive() {
-	return window.__TAURI_INVOKE__("get_courses_active").then((courses) => {
+	return invoke("get_courses_active").then((courses) => {
 		if (Array.isArray(courses)) {
 			for (const course of courses) {
 				if (course.Ok) {
@@ -48,35 +50,35 @@ export function getCourse(uuid) {
 function normalizeCourse(course) {
 	if (course.books) {
 		for (const book of course.books) {
-			book.file = window.__TAURI__.convertFileSrc(book.file);
+			book.file = convertFileSrc(book.file);
 		}
 	}
 }
 
 export function setCourseCompletion(uuid, completion) {
-	return window.__TAURI_INVOKE__("set_course_completion", {
+	return invoke("set_course_completion", {
 		id: uuid,
 		data: completion
 	});
 }
 
 export function setCourseActiveStatus(uuid, active) {
-	return window.__TAURI_INVOKE__("get_course_active_status", {
+	return invoke("get_course_active_status", {
 		id: uuid,
 		data: active
 	});
 }
 
 export function getOverallProgress() {
-	return window.__TAURI_INVOKE__("get_overall_progress");
+	return invoke("get_overall_progress");
 }
 
 export function getSettings() {
-	return window.__TAURI_INVOKE__("get_settings");
+	return invoke("get_settings");
 }
 
 export function setSettings(settings) {
-	return window.__TAURI_INVOKE__("set_settings", {
+	return invoke("set_settings", {
 		data: settings
 	});
 }
