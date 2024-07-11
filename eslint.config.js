2205b263f11eab49fefca115ad9a3191ab6cfa56
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-11 11:41:24 -0700

    Fixed dependency conflict

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
+// @ts-check
+
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
 	{ files: ["**/*.{js,mjs,cjs,ts}"] },
 	{ languageOptions: { globals: globals.browser } },
-	pluginJs.configs.recommended,
+	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	{
 		rules: {
