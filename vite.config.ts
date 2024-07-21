9318710637b34380a92b4896150f315888a00e3b
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-21 05:10:07 -0700

    Bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,7 @@ export default defineConfig({
 				main: resolve(__dirname, "index.html"),
 				viewer: resolve(__dirname, "viewer.html"),
 				error: resolve(__dirname, "error.html"),
+				navigator: resolve(__dirname, "navigator.html"),
 			},
 		},
 		chunkSizeWarningLimit: 550,
