89a31a153108134b518fb7de6b24bdbe8ebe6b9f
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-25 01:20:02 -0700

    Started work on Course Navigator

--- src/navigator/navigator.ts
+++ src/navigator/navigator.ts
@@ -1,14 +1,12 @@
 import {
+	Course,
+	CourseMap,
+	CourseProgress,
 	displayError,
 	getActiveCourses,
 	getAll,
-	getSettings,
 	openDataDir,
 } from "../bindings.ts";
-
-const settingsPromise = getSettings().catch((error) => {
-	displayError(error);
-});
 
 const listingPromise = getAll().catch((error) => {
 	displayError(error);
@@ -19,6 +17,8 @@ const activePromise = getActiveCourses().catch((error) => {
 });
 
 const folderButton = document.getElementById("folderOpener");
+const refreshButton = document.getElementById("refreshButton");
+const contentListing = document.getElementById("listingInner");
 
 function openFolder() {
 	openDataDir().catch((error) => {
@@ -31,10 +31,118 @@ if (folderButton) {
 	folderButton.addEventListener("keydown", openFolder);
 }
 
-const settings = await settingsPromise;
-const listing = await listingPromise;
-const activeCourses = await activePromise;
+if (refreshButton) {
+	refreshButton.addEventListener("click", () => {
+		location.reload();
+	});
+	refreshButton.addEventListener("keydown", () => {
+		location.reload();
+	});
+}
 
-console.log("settings", settings);
-console.log("listing", listing);
-console.log("active", activeCourses);
+if (contentListing) {
+	listingPromise.then(async (listing) => {
+		if (!listing) {
+			return;
+		}
+
+		const fragment = document.createDocumentFragment();
+
+		if (listing.course_maps.length != 0 && listing.courses.length != 0) {
+			fragment.appendChild(buildCourseMapListing(listing.course_maps));
+		}
+
+		const activeCourses = await activePromise;
+		if (activeCourses) {
+			fragment.appendChild(
+				buildCourseListing(listing.courses, new Set(activeCourses)),
+			);
+		}
+
+		contentListing.innerHTML = "";
+		contentListing.appendChild(fragment);
+
+		console.log("listing", listing);
+
+		console.log("active", activeCourses);
+	});
+}
+
+function buildCourseMapListing(
+	courseMaps: [CourseMap, string][],
+): DocumentFragment {
+	const fragment = document.createDocumentFragment();
+
+	const header = document.createElement("h2");
+	header.innerText = "Your Course Maps";
+
+	fragment.appendChild(header);
+
+	const list = document.createElement("ul");
+
+	for (const [courseMap, _] of courseMaps) {
+		const element = document.createElement("li");
+		element.innerText = courseMap.title;
+
+		list.appendChild(element);
+	}
+
+	fragment.appendChild(list);
+
+	if (courseMaps.length != 0) {
+		fragment.appendChild(document.createElement("br"));
+	}
+
+	return fragment;
+}
+
+function buildCourseListing(
+	courses: [Course, CourseProgress][],
+	active: Set<string>,
+): DocumentFragment {
+	const fragment = document.createDocumentFragment();
+
+	if (courses.length == 0) {
+		const header = document.createElement("h2");
+		header.innerText = "No courses found!";
+
+		fragment.appendChild(header);
+
+		const message = document.createElement("span");
+		message.innerText =
+			"Try importing some courses into the resource folder, and then refreshing the navigator.";
+
+		fragment.appendChild(message);
+
+		return fragment;
+	}
+
+	const header = document.createElement("h2");
+	header.innerText = "Your Courses";
+
+	fragment.appendChild(header);
+
+	const list = document.createElement("ul");
+
+	for (const [course, _courseProgress] of courses) {
+		const element = document.createElement("li");
+		element.id = "course-" + course.uuid;
+
+		const label = document.createElement("span");
+		label.innerText = course.title;
+		element.appendChild(label);
+
+		const checkbox = document.createElement("input");
+		checkbox.setAttribute("type", "checkbox");
+		if (active.has(course.uuid)) {
+			checkbox.checked = true;
+		}
+		element.appendChild(checkbox);
+
+		list.appendChild(element);
+	}
+
+	fragment.appendChild(list);
+
+	return fragment;
+}
