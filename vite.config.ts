6f1ec17e9e02e007f194717f9535b08146343726
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-07-24 20:57:56 -0700

    Updated package configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 import legacy from "@vitejs/plugin-legacy";
 import { createWriteStream, existsSync, mkdirSync } from "fs";
 
@@ -31,8 +33,6 @@ export default defineConfig({
 		"TAURI_DEBUG",
 	],
 	build: {
-		// Tauri uses Edge on Windows and WebKit on macOS and Linux
-		target: process.env.TAURI_PLATFORM == "windows" ? "edge109" : "safari12",
 		// don't minify for debug builds
 		minify: !process.env.TAURI_DEBUG ? "terser" : false,
 		cssMinify: "lightningcss",
@@ -46,21 +46,27 @@ export default defineConfig({
 				navigator: resolve(__dirname, "navigator.html"),
 			},
 		},
-		chunkSizeWarningLimit: 550,
 	},
 	css: {
 		transformer: "lightningcss",
+		lightningcss: {
+			targets: browserslistToTargets(
+				browserslist(
+					process.env.TAURI_PLATFORM == "windows" ? "edge>=89" : "safari>=12",
+				),
+			),
+		},
 	},
 	plugins: [
 		eslint(),
 		legacy({
+			// Tauri uses Edge on Windows and WebKit on macOS and Linux
 			targets:
-				process.env.TAURI_PLATFORM == "windows" ? "edge>=109" : "safari>=12",
+				process.env.TAURI_PLATFORM == "windows" ? "edge>=89" : "safari>=12",
 			additionalLegacyPolyfills: [],
+			// Consider browser versions released since 2023 as modern
 			modernTargets:
-				process.env.TAURI_PLATFORM == "windows"
-					? "last 10 Edge versions"
-					: "safari>=15.6",
+				process.env.TAURI_PLATFORM == "windows" ? "edge>=109" : "safari>=16.3",
 		}),
 	],
 });
