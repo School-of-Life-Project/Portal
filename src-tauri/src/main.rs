b0a1d4086b681d11b5c0d12f8cbb36261e7eb57e
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-08-26 09:48:03 -0700

    Added scaffolding for mobile support

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,41 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![warn(clippy::pedantic)]
 
-use anyhow::{Context, Result};
-use tauri::Manager;
-
-mod api;
-mod course;
-mod progress;
-
-pub const MAX_FS_CONCURRENCY: usize = 8;
-
-const PROJECT_ISSUE_TRACKER: &str = "https://github.com/School-of-Life-Project/Portal-App/issues";
-const PROJECT_ISSUE_TRACKER_NEW: &str =
-    "https://github.com/School-of-Life-Project/Portal-App/issues/new";
-const PROJECT_SOURCE_REPO: &str = "https://github.com/School-of-Life-Project/Portal-App";
-
-fn main() -> Result<()> {
-    tauri::Builder::default()
-        .setup(|app| {
-            app.manage(api::State::new(app.path().app_data_dir()?));
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![
-            api::open_data_dir,
-            api::open_internal_data_dir,
-            api::open_project_issue_tracker,
-            api::open_project_repo,
-            api::get_course,
-            api::set_course_completion,
-            api::get_active_courses,
-            api::set_active_courses,
-            api::get_all,
-            api::get_active,
-            api::get_overall_progress,
-            api::get_settings,
-            api::set_settings,
-        ])
-        .run(tauri::generate_context!())
-        .context("Failed to initalize application window")
+fn main() -> anyhow::Result<()> {
+    app_lib::run()
 }
