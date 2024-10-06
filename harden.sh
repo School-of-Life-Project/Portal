d5a64d561618a8432ee6101c877500dac367ff5d
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-10-06 12:07:54 -0700

    Tweaked hardening script

--- harden.sh
+++ harden.sh
@@ -1,5 +1,5 @@
 if [ -f src-tauri/tauri-hardened.json ]; then
-	rm src-tauri/tauri.conf.json
+	mv src-tauri/tauri.conf.json src-tauri/tauri.conf.json.old
 	mv src-tauri/tauri-hardened.json src-tauri/tauri.conf.json
 
 	echo "Successfully applied application hardening. Rerun the compiler to create a hardened build."
